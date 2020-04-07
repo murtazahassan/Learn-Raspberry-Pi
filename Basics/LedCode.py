359f4ce66771e8fde6f5e91f677b742ba4a70dd1
murtazahassan <mhpr@live.co.uk>
2020-04-07 04:50:19 +0300

    Create LedCode.py

---
+++ Basics/LedCode.py
@@ -0,0 +1,12 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
+
+ ''' led.toggle()   // to switch between states ''' 
